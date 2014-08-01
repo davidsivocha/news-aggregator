064311035e500fd117fcbeb8d00d463ff580bb12
davidsivocha <david@sivocha.com>
2014-08-01 10:20:33 +0000

    Added Validation rules to Feeds Model

--- app/models/Feeds.php
+++ app/models/Feeds.php
@@ -2,4 +2,11 @@
 class Feeds extends Eloquent {
     protected $table = 'feeds';
     protected $fillable = array('feed', 'title', 'active', 'category');
+    
+    public static $form_rules = array(
+    	'feed'		=> 'required|url|active_url',
+        'title'		=> 'required',
+        'active'	=> 'required|between:0:1',
+        'category'	=> 'required| in:News,Esports,Technology'
+    );
 }
