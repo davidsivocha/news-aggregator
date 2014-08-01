8eb335cb0c8607bd82a8b12327e3725e4fa4a60d
davidsivocha <david@sivocha.com>
2014-08-01 10:35:47 +0000

    Add custom parse_feed function in overriden Illuminate\Support\Str

--- app/lib/Support/Str.php
+++ app/lib/Support/Str.php
@@ -1,5 +1,18 @@
 <?php
 namespace app\lib\Support;
 class Str extends \Illuminate\Support\Str {
-    
+	public static function parse_feed($url){
+        $feed = simplexml_load_file($url);
+        if(!count($feed)){
+            return array();
+        } else {
+            $out = array();
+            $items = $feed->channel->item;
+            for($i=0;$i<5;$i++){
+                $out[] = $items[$i];
+            }
+            return $out;
+        }
+            
+    }   
 }
